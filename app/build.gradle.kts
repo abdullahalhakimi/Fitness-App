9f3c1f63000e314fd6de1bebba9c4ff7ceb084c6
abdullahalhakimi <abdullahalhakimi7@gmail.com>
2024-07-20 08:12:53 +0300

    add translation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,8 +66,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation(libs.androidx.navigation.compose)
 
     // System Ui color
-    implementation("com.google.accompanist:accompanist-systemuicontroller:0.30.0")
+    implementation(libs.accompanist.systemuicontroller)
 }
